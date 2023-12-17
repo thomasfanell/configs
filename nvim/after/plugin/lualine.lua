829f2ed072f5b3593eb577e8326598c780c4ac7c
Thomas Fanell <thomas@fanell.dev>
2023-12-17 12:12:16 +0100

    Disable icons in lualine and set the component separator.

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,1 +1,7 @@
-require("lualine").setup({})
+require("lualine").setup({
+    options = {
+        icons_enabled = false,
+        component_separators = "|",
+        section_separators = "",
+      },
+})
