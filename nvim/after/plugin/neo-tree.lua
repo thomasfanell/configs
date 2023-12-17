a7a607626ffcb1a6ce521c75ade8998a1e0b47e2
Thomas Fanell <thomas@fanell.dev>
2023-12-17 12:11:26 +0100

    Show dotfiles in neo-tree.

--- nvim/after/plugin/neo-tree.lua
+++ nvim/after/plugin/neo-tree.lua
@@ -1,2 +1,10 @@
+require("neo-tree").setup {
+    filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+        }
+    }
+}
+
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
 vim.keymap.set('n', '<leader>bf', ':Neotree buffers reveal float<CR>', {})
